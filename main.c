0850a003230a2b7b484e8f62a10ce524c5e0b939
akandadai <anantkandadai@gmail.com>
2020-07-18 13:48:37 -0400

    define system stats

--- main.c
+++ main.c
@@ -8,6 +8,13 @@
 
 osMessageQueueId_t queues[N_QUEUE];
 
+struct{
+    int total_q; //total messages queued
+    int total_p; //total messages processed
+    int total_d; //total messages denied (due to full queue)
+} system_stats = {0, 0, 0};
+
+
 uint32_t interarrival_ticks(uint32_t seconds_rate){
 		return (next_event()/seconds_rate*osKernelGetTickFreq()) >> 16;
 }
