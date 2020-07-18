d7f2b3fc20419949b8c3719b5349ba903263b9c4
akandadai <anantkandadai@gmail.com>
2020-07-18 13:35:40 -0400

    queue initiliazation

--- main.c
+++ main.c
@@ -3,9 +3,19 @@
 #include "random.h"
 #include <stdbool.h>
 
+#define N_QUEUE 2
+#define QLIMIT 10
+
+osMessageQueueId_t queues[N_QUEUE];
 
 uint32_t interarrival_ticks(uint32_t seconds_rate){
 		return (next_event()/seconds_rate*osKernelGetTickFreq()) >> 16;
+}
+
+void initialize_queues(void){
+    for(int i =0; i < N_QUEUE; i++){
+        queues[i] = osMessageQueueNew(QLIMIT, sizeof(int), NULL);
+    }
 }
 
 void arrivals(void* args);
